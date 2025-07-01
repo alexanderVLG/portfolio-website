c643e2a209207191e9cd9f17504f95647affe372
Alexander Kovalev <40559899+alexanderVLG@users.noreply.github.com>
2025-07-01 08:16:37 +0500

    настраиват слайдер

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 new Glide('.glide', {
   type: 'carousel',  // 'slider' (не зацикленный) или 'carousel' (бесконечный)
   startAt: 0,        // Начинать с первого слайда
-  perView: 1,        // Сколько слайдов показывать одновременно
-  autoplay: 2000,    // Автопрокрутка (мс)
-  hoverpause: true,  // Пауза при наведении
+  perView: 3,        // Сколько слайдов показывать одновременно
+  autoplay: 5000,    // Автопрокрутка (мс)
+  hoverpause: true,
+  breakpoints: {
+     1200: {
+		perView: 2,
+	 },
+     767: {
+		perView: 1
+	 }
+  }  // Пауза при наведении
 }).mount();
